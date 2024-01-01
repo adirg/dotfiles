0bea1383c35d84382b6fb7be8efa65bdec7b33c4
Adir Gabai <adirgabai@gmail.com>
2024-01-01 15:05:34 -0500

    nvim: bumping telescope version

--- dotfiles/.config/nvim/lua/plugins/init.lua
+++ dotfiles/.config/nvim/lua/plugins/init.lua
@@ -91,7 +91,7 @@ return require('packer').startup(function(use)
     -- Extendable fuzzy finder
     use{
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.0',
+        tag = '0.1.4',
         requires = 'nvim-lua/plenary.nvim',
         config = config('telescope'),
     }
