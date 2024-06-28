36966449e18206f3f3614618970d6342b71af8e2
Adir Gabai <adirgabai@gmail.com>
2024-06-28 12:05:08 -0400

    nvim: fix ']d' mapping

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -6,6 +6,6 @@ vim.keymap.set('', 'k', 'gk')
 -- See `:help vim.diagnostics.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
