acb33b71e5e4bb6a2e220e4b5dc597da5e7f006c
Adir Gabai <adirgabai@gmail.com>
2024-06-28 12:30:49 -0400

    fish: enhance config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,26 @@
+set -g fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if test -e /opt/rh/gcc-toolset-10/enable
+        bash /opt/rh/gcc-toolset-10/enable
+    end
+end
+
+function fish_prompt
+    # metadata line
+    printf "%s[%s]%s" (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    printf "%s%s%s" (set_color red) (fish_git_prompt) (set_color normal)
+
+    printf "\n"
+
+    # command line
+    if set -q SSH_CLIENT; or set -q SSH_CONNECTION
+        printf "%s%s@%s" (set_color brblue) $USER $hostname
+    end
+    printf "%s > " (set_color normal)
+end
+
+if command -q direnv
+    direnv hook fish | source
 end
