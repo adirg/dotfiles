09b194d3c791c3ed213c1b786cf74824d7ef485b
Adir Gabai <adirgabai@gmail.com>
2024-06-28 10:49:29 -0400

    nvim: use gruvbox with hard contrast

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -4,6 +4,9 @@ return {
         'ellisonleao/gruvbox.nvim',
         priority = 1000,
         config = function()
+            require("gruvbox").setup({
+                contrast = "hard"
+            })
             vim.cmd.colorscheme 'gruvbox'
         end
     },
