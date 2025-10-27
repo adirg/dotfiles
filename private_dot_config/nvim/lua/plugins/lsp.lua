893850af1a357ed2843747ab953c501a05ef9432
Adir Gabai <adirgabai@gmail.com>
2025-10-26 20:31:30 -0400

    nvim/lsp: add borders to floating windows

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -78,6 +78,13 @@ local function config()
             },
         },
     })
+
+    local orig_open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or 'rounded'
+        return orig_open_floating_preview(contents, syntax, opts, ...)
+    end
 end
 
 return {
