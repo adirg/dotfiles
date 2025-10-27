bb44aed6941fa8bd17b764538f30611eff5578d9
Adir Gabai <adirgabai@gmail.com>
2025-10-26 20:38:21 -0400

    fish/user_key_bindings: enable vi mode and ctrl+p / ctrl+n

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,9 @@
 function fish_user_key_bindings
-  # fzf_key_bindings
+  fish_vi_key_bindings
+
+  bind \cp 'up-or-search'
+  bind \cn 'down-or-search'
+
+  bind -M insert \cp 'up-or-search'
+  bind -M insert \cn 'up-or-search'
 end
